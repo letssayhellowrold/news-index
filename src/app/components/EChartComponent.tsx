b4e0d958f9ee6601880a6e2d506bfd9c00be4980
ostracism <202211079252@mail.bnu.edu.cn>
2024-10-31 15:53:19 +0800

    修改特殊事件显示效果

--- src/app/components/EChartComponent.tsx
+++ src/app/components/EChartComponent.tsx
@@ -58,35 +58,104 @@ const LineChartComponent: React.FC = () => {
         .map((point) => ({
           coord: [data.x[point.dataIndex], point.value],
           label: {
-            show: true,
-            position: "inside",
-            formatter: point.name,
-            fontSize: "auto",
+            position: "outside",
+            fontSize: "15px",
             textAlign: "auto",
+            color: "#FFF",
+            formatter: () => {
+              return "{Bg|" + point.name + "}";
+            },
+            rich: {
+              Bg: {
+                backgroundColor: "sky",
+                borderRadius: 4,
+                padding: [5, 5],
+              },
+            },
           },
+          symbol: "circle", // 设置特殊点的形状为圆形
+          symbolSize: 10, // 设置特殊点的大小
           itemStyle: {
-            color: "#f0f0f0",
+            color: "#00008B", // 设置特殊点的颜色为深蓝色
           },
+          z: 100,
         }));
 
       const option = {
-        animation: false,
         legend: {
+          orient: "horizontal", //图例布局方式：水平 'horizontal' 、垂直 'vertical'
+          x: "center", // 横向放置位置，选项：'center'、'left'、'right'、'number'（横向值 px）
+          y: "top", // 纵向放置位置，选项：'top'、'bottom'、'center'、'number'（纵向值 px）
           data: ["index"],
         },
         title: {
-          text: "linechart",
+          text: "News Index",
         },
         tooltip: {
           trigger: "axis",
+          axisPointer: {
+            // 坐标轴指示器配置项。
+            type: "cross", // 'line' 直线指示器  'shadow' 阴影指示器  'none' 无指示器  'cross' 十字准星指示器。
+            axis: "auto", // 指示器的坐标轴。
+            snap: true, // 坐标轴指示器是否自动吸附到点上
+          },
+          showContent: true,
+        },
+        axisPointer: {
+          link: { xAxisIndex: "all" },
+          label: {
+            backgroundColor: "#777",
+          },
+        },
+        toolbox: {
+          feature: {
+            dataZoom: {
+              yAxisIndex: false,
+            },
+            brush: {
+              type: ["lineX", "clear"],
+            },
+            saveAsImage: {}, //下载按钮
+          },
+        },
+        brush: {
+          xAxisIndex: "all",
+          brushLink: "all",
+          outOfBrush: {
+            colorAlpha: 0.1,
+          },
         },
         xAxis: {
           type: "category",
           data: data.x,
+          scale: true,
+          boundaryGap: false,
+          axisLine: { onZero: false },
+          splitLine: { show: false },
+          splitNumber: 20,
         },
         yAxis: {
           type: "value",
+          splitArea: {
+            show: true,
+          },
         },
+        dataZoom: [
+          {
+            type: "inside",
+            xAxisIndex: [0],
+            start: 0,
+            end: 100,
+          },
+          {
+            show: true,
+            xAxisIndex: [0, 1],
+            type: "slider",
+            top: "90%",
+            start: 0,
+            end: 100,
+          },
+        ],
         series: [
           {
             name: "index",
@@ -95,6 +164,14 @@ const LineChartComponent: React.FC = () => {
             showSymbol: false,
             markPoint: {
               data: marks,
+              label: {
+                normal: {
+                  show: false, // 默认不显示
+                },
+                emphasis: {
+                  show: true, // 鼠标悬浮时显示
+                },
+              },
             },
           },
         ],
